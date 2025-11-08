750e2a2bbc0bd5e09fb58c5ab0ef413bf8c71ba2
허윤아 <aquahua@gmail.com>
2025-11-09 01:32:58 +0900

    [�Feature - base.html 및 페이지 파일들 생성

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from .views import IndexView
+from . import views
 
 app_name = "frontend"
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index"),
+    path("citizen/", views.CitizenView.as_view(), name="citizen"),
+    path("community/", views.CommunityView.as_view(), name="community"),
+    path("emotional/", views.EmotionalView.as_view(), name="emotional"),
+    path("health/", views.HealthView.as_view(), name="health"),
+    path("", views.HomeView.as_view(), name="home"),
+    path("lifestyle/", views.LifestyleView.as_view(), name="lifestyle"),
+    path("local/", views.LocalView.as_view(), name="local"),
+    path("society/", views.SocietyView.as_view(), name="society"),
 ]
 
--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,27 @@
 from django.views.generic import TemplateView
 
+class HomeView(TemplateView):
+    template_name = "home.html"
 
-class IndexView(TemplateView):
-    template_name = "index.html"
+class LifestyleView(TemplateView):
+    template_name = "lifestyle.html"
+
+class SocietyView(TemplateView):
+    template_name = "society.html"
+
+class CitizenView(TemplateView):
+    template_name = "citizen.html"
+
+class CommunityView(TemplateView):
+    template_name = "community.html"
+
+class EmotionalView(TemplateView):
+    template_name = "emotional.html"
+
+class HealthView(TemplateView):
+    template_name = "health.html"
+
+class LocalView(TemplateView):
+    template_name = "local.html"
+
 
